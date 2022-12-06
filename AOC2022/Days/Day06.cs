cce31031e7e2254e164064efd4e3258ec06ee4c7
Patrick Toft Steffensen <17211264+PTST@users.noreply.github.com>
2022-12-06 08:12:05 +0100

    Day06

--- AOC2022/Days/Day06.cs
+++ AOC2022/Days/Day06.cs
@@ -8,19 +8,49 @@ namespace AOC2022.Days
     internal class Day06 : IDay
     {
         private string Input => Helper.GetDay(6);
-
+        private Device device = new Device();
         public Day06()
         {
         }
 
         public void Part01()
         {
-            Console.WriteLine($"{this.GetType().Name} - Part 1: Not implemented");
+            var marker = this.device.FindStartOfPackageMarker(this.Input.ToCharArray());
+            Console.WriteLine($"{this.GetType().Name} - Part 1: {marker}");
         }
 
         public void Part02()
         {
-            Console.WriteLine($"{this.GetType().Name} - Part 2: Not implemented");
+            var marker = this.device.FindStartOfMessageMarker(this.Input.ToCharArray());
+            Console.WriteLine($"{this.GetType().Name} - Part 2: {marker}");
+        }
+
+        class Device{
+            public int FindStartOfPackageMarker(char[] input)
+            {
+                for (int i = 3; i < input.Length; i++)
+                {
+                    var chars = input[(i - 3)..(i+1)];
+                    if (chars.Distinct().Count() == 4)
+                    {
+                        return i + 1;
+                    }
+                }
+                throw new Exception("No marker found");
+            }
+
+            public int FindStartOfMessageMarker(char[] input)
+            {
+                for (int i = 13; i < input.Length; i++)
+                {
+                    var chars = input[(i - 13)..(i + 1)];
+                    if (chars.Distinct().Count() == 14)
+                    {
+                        return i + 1;
+                    }
+                }
+                throw new Exception("No marker found");
+            }
         }
     }
 }
